658f8c51cad6b534b903676cfb583353ff4a9579
Luiza <luizacpacheco@poli.ufrj.br>
2019-11-20 18:29:00 -0300

    Questão 1 corrigida

--- Q1.py
+++ Q1.py
@@ -1,31 +1,40 @@
-Data = {
-    "Ano" : 1111
-    "Mes" : 11
-    "Dia" : 11
-    }
-def Q1A (data)
-    Data["Ano"] = Toint(data[0] + data[1] + data[2] + data[3])
-    Data["Mes"] = Toint(data[5] + data[6])
-    Data["Dia"] = Toint(data[8] + data[8])
-    retorno = Data
+def Q1A (data):
+    retorno = {"Ano": int(data[0] + data[1] + data[2] + data[3]), "Mes": int(data[5] + data[6]), "Dia": int(data[8] + data[9])}
     return (retorno)
-def Q1B (Ativo)
-    if(len(ativo) == 4)
-        ativo.pop()
-        ativo.pop()
+def Q1B (ativo):
+    if(len(ativo) == 4):
+        ativo = ativo[0] + ativo[1] + ativo[2]
     retorno = (ativo == "SIM")
     return retorno
-def Q1C (String)
+def Q1C (String):
     retorno = list(String.split(":"))
     return retorno
-def Q1D (descritor)
+def Q1D (descritor):
     descritor.seek(0)
-    lista = list(descritor.readlines())
-    return (lista)
-def main()
+    linhas = list(descritor.readlines())
+    return (linhas)
+def main():
     arquivo = open("cadastro.txt", "r")
-    lista = Q1D(arquivo)
-    for i in range (len(lista))
-        lista_cad = Q1C(lista[i])
+    linhas = Q1D(arquivo)
+    arquivo.close()
+    Info = {
+        "CPF":"",
+        "NOME":"",
+        "DATA_DE_NASCIMENTO":"",
+        "DATA_DO_CADASTRO":"",
+        "ATIVO":True
+        }
+    Dic_list = [" "]*len(linhas)
+    for i in range (len(linhas)):
+        dados = Q1C(linhas[i])
+        cadastro = Info.copy()
+        cadastro["CPF"] = dados[0]
+        cadastro["NOME"] = dados[1]
+        cadastro["DATA_DE_NASCIMENTO"] = Q1A(dados[2])
+        cadastro["DATA_DO_CADASTRO"] = Q1A(dados[3])
+        cadastro["ATIVO"] = Q1B(dados[4])
+        Dic_list[i] = cadastro
+        
+    print(Dic_list)
 
 main()
