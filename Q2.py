788cdaacca5a242b4066c086f537c497946a6656
Luiza <luizacpacheco@poli.ufrj.br>
2019-11-20 17:26:44 -0300

    Questão 2 corrigida

--- Q2.py
+++ Q2.py
@@ -1,26 +1,31 @@
-def Q2A (matriz)
+def Q2A (matriz):
     detA = matriz[0][0]*matriz[1][1] - matriz[1][0]*matriz[0][1]
-    if(detA == 0)
-        retorno = "none"
-    else
-        retorno = list(matriz)
+    if(detA == 0):
+        retorno = None
+    else:
+        retorno = [0]*2
+        retorno[1] = [0]*2
+        retorno[0] = [0]*2
         retorno[0][0] = matriz[1][1]/detA
         retorno[1][1] = matriz[0][0]/detA
         retorno[1][0] = matriz[1][0]*(-1)/detA
         retorno[0][1] = matriz[0][1]*(-1)/detA
     return retorno
-def main()
-    matriz = [" "]*2
-    matriz[1] = [" "]*2
-    matriz[0] = [" "]*2
-    matriz[0][0] = int(input("a"))
-    matriz[0][1] = int(input("b"))
-    matriz[1][0] = int(input("c"))
-    matriz[1][1] = int(input("d"))
+def main():
+    matriz = [0]*2
+    matriz[1] = [0]*2
+    matriz[0] = [0]*2
+    matriz[0][0] = float(input("a: "))
+    matriz[0][1] = float(input("b: "))
+    matriz[1][0] = float(input("c: "))
+    matriz[1][1] = float(input("d: "))
+    print(str(matriz[0][0]) + "\t" + str(matriz[0][1]) + "\n" + str(matriz[1][0]) + "\t" + str(matriz[1][1]))
     nova = Q2A(matriz)
-    print(matriz[0][0] + "\t" + matriz[0][1] + "\n" + matriz[1][0] + "\t" + matriz[1][1])
     print()
-    print(nova[0][0] + "\t" + nova[0][1] + "\n" + nova[1][0] + "\t" + nova[1][1])
+    if(nova == None):
+        print("None")
+    else:
+        print(str(nova[0][0]) + "\t" + str(nova[0][1]) + "\n" + str(nova[1][0]) + "\t" + str(nova[1][1]))
 
 main()
 
